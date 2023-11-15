b141d8854cc896bf19d6c46dec8078c369810f14
resandadezcaasyam <dezca30@gmail.com>
2023-11-15 15:59:41 +0700

    Fixing Lagi

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 
-
+# Set the root URL configuration.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
